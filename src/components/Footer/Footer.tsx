7a47e9340619de481d43d3a439b57567d4743b19
Ostrynska <kateriinag@gmail.com>
2024-12-11 15:39:01 +0200

    fixed styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SiRefinedgithub } from "react-icons/si";
 import styles from './Footer.module.scss';
 
 const Footer: React.FC = () => {
@@ -6,6 +7,14 @@ const Footer: React.FC = () => {
     <footer className={styles.footer}>
       <div className={styles.content}>
         <p className={styles.text}>Made for Come Back Agency</p>
+        <a 
+              href="https://github.com/Ostrynska/weatherly-app" 
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className={styles.link}
+            >
+              <SiRefinedgithub className={styles.iconButton} />
+            </a>
       </div>
     </footer>
   );
